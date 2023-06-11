a50992af94bc510241bf3118083f48fd1a003aca
Chamuth Chamandana <c.chamandana@gmail.com>
2023-06-12 00:04:22 +0530

    add handshake logic

--- src/core/Connection.ts
+++ src/core/Connection.ts
@@ -9,17 +9,21 @@ export default class Connection {
   private client: net.Socket;
   private queryQueue: QueryQueue;
   private connectionState: ConnectionState = ConnectionState.NotConnected;
+  private settings: ConnectionSettings;
 
   /**
    * Initialize connection with established socket
    * @param client Client socket
    */
   constructor(settings: ConnectionSettings) {
+    this.settings = settings;
+
     this.client = new net.Socket();
     this.connectionState = ConnectionState.Connecting;
 
     this.client.connect(settings.port, settings.host, () => {
       this.connectionState = ConnectionState.Connected;
+      this.handShake();
     });
 
     this.client.on("data", (data) => {
@@ -32,6 +36,36 @@ export default class Connection {
 
     // Initialize query queue
     this.queryQueue = new QueryQueue();
+  }
+
+  /**
+   * Perform authorization handshake with the given credentials
+   */
+  private handShake() {
+    const requestId = uuidv4();
+
+    this.client.write(
+      JSON.stringify({
+        requestId,
+        query: {},
+        credentials: {
+          username: this.settings.username,
+          password: this.settings.password,
+        },
+      }),
+    );
+
+    console.log("WRITING HANDSHAKE");
+
+    this.queryQueue.enqueue({
+      requestId,
+      callback: () => {
+        console.log("HANDSHAKE RESPONSE");
+      },
+      errorCallback: () => {
+        console.log("HANDSHAKE ERROR");
+      },
+    });
   }
 
   /**
